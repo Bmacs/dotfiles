50c5ff591fcb527d15e0346a209b4cd2ec977b1a
Brendan McNamara <brendancmcnamara@gmail.com>
2021-12-14 00:32:04 -0500

    Added spotify scrolling and made setup executable

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s dotfiles/.bash_profile ~/.bash_profile
+ln -s dotfiles/.zshrc ~/.zshrc
+ln -s dotfiles/.imwheelrc ~/.imwheelrc
+ln -s dotfiles/.xinitrc ~/.xinitrc
+ln -s dotfiles/.gitconfig ~/.gitconfig
